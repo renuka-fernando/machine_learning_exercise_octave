4cc414bff299e1209f5167c97b24a996f3b53b25
Renuka Fernando <renuka.piyumal@gmail.com>
2018-05-31 20:54:11 +0530

    regularized gradient

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -45,8 +45,8 @@ J = 0.5 * sum(sum((X * Theta' .* R - Y .* R) .^ 2)) ...
 
 
 
-X_grad = (X * Theta' .* R - Y .* R) * Theta;
-Theta_grad = (X * Theta' .* R - Y .* R)' * X;
+X_grad = (X * Theta' .* R - Y .* R) * Theta + (lambda * X);
+Theta_grad = (X * Theta' .* R - Y .* R)' * X + (lambda * Theta);
 
 
 
